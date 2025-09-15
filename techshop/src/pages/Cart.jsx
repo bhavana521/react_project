c6a5ca3eaa48979d3c7b871c81f960b5bc75ab28
bhavana <bhavanachowdary2004@gmail.com>
2025-09-15 17:39:04 +0530

    comment

--- techshop/src/pages/Cart.jsx
+++ techshop/src/pages/Cart.jsx
@@ -27,8 +27,8 @@ const Cart = () => {
           <h1 className="text-center text-white text-4xl font-bold pt-18 fhfh">
             Your cart is empty
           </h1>
-          <div className="w-full flex justify-center mt-4 ghhh htijhdfiohj">
-            <Link to="/Allproduct" className="qpqp"  hghjhbbvbh bhavana hvbbv>
+          <div className="w-full flex justify-center mt-4 comment">
+            <Link to="/Allproduct" className="qpqp"  >
               <button className="bg-red-500 border rounded-4 text-4xl flex text-white fonbol items-center gap-5 font-bold py-4 px-8">
                 <FaArrowLeftLong />
                 Continue Shopping
