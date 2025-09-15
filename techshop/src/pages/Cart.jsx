d8e047af618e4ccddac166a2b5c3467e1ef41557
bhavana <bhavanachowdary2004@gmail.com>
2025-09-15 17:33:42 +0530

    responsive

--- techshop/src/pages/Cart.jsx
+++ techshop/src/pages/Cart.jsx
@@ -27,7 +27,7 @@ const Cart = () => {
           <h1 className="text-center text-white text-4xl font-bold pt-18 fhfh">
             Your cart is empty
           </h1>
-          <div className="w-full flex justify-center mt-4 ghhh">
+          <div className="w-full flex justify-center mt-4 ghhh htijhdfiohj">
             <Link to="/Allproduct" className="qpqp" >
               <button className="bg-red-500 border rounded-4 text-4xl flex text-white fonbol items-center gap-5 font-bold py-4 px-8">
                 <FaArrowLeftLong />
